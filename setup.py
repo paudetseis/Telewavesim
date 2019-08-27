ce0bba3c3c135b6ec68dd4057297445c35eed40a
Pascal Audet <pascal.audet@uottawa.ca>
2019-08-27 15:57:56 -0400

    Jupyter Notebooks and updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 from numpy.distutils.core import setup, Extension
 
+version=open('version.txt').read().split()[0]
 
 ext = [Extension(name='telewavesim.rmat_f',
                  sources=['src/rmat.f90', 'src/rmat_sub.f90'],
@@ -9,18 +10,26 @@ ext = [Extension(name='telewavesim.rmat_f',
 
 setup(
   name = 'telewavesim',
-  version = '0.0.1',
+  version = version,
   description = 'Python package for teleseismic body-wave modeling',
-  author = 'Pascal Audet',
+  author = 'Pascal Audet, Colin Thomson',
+  maintainer = 'Pascal Audet',
   author_email = 'pascal.audet@uottawa.ca',
   url = 'https://gitlab.com/uottawa-geophysics/SeismoPy/TeleWaveSim', 
   classifiers = [
-    'Development Status :: 1 - Alpha',
+    'Development Status :: 0 - Alpha',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Fortran',
     'Programming Language :: Python :: 3.7'],
-  install_requires=["numpy>=1.16", "obspy>=1.1.0", "pyfftw>=0.11.1", "matplotlib"],
-  python_requires='>=3.7',
+  install_requires = ["numpy>=1.16", "obspy>=1.1.0", "pyfftw>=0.11.1", "matplotlib"],
+  python_requires = '>=3.7',
   ext_modules = ext,
-  packages=['telewavesim']
+  packages = ['telewavesim'],
+  package_data = {
+    "telewavesim": [
+      "examples/*.ipynb",
+      "examples/models/*.txt",
+      "examples/Notebooks/Python/*.ipynb",
+      "examples/Notebooks/Fortran/*.ipynb"]
+  }
 )
