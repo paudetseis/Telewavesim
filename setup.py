3de6bd8f73a2d0bfc5a24fa8a91f69784708382e
Pascal Audet <pascal.audet@uottawa.ca>
2025-01-31 13:56:28 +1300

    first refactoring toward using sckit-build-core

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import re
 from numpy.distutils.core import setup, Extension
 from numpy.distutils.system_info import get_info
 
+
 def find_version(*paths):
     fname = os.path.join(os.path.dirname(__file__), *paths)
     with open(fname, encoding='utf-8') as fp:
